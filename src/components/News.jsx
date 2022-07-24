85fb2fcadd69a1228193e7d11a82d400ad4d8573
hamzaplojovic <hamzaplojovic4@gmail.com>
2022-07-24 17:14:51 +0200

    commit

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import "./Styles/News.css";
+import "./Styles/News.scss";
 import Header from "./Header";
 
 function News() {
